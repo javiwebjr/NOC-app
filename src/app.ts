b7197d7db459fa8ed6e8ed8598b3a7c0441e4aa1
javiwebjr <urrutia_987@hotmail.com>
2024-03-08 14:57:01 -0600

    MongoDB + PostgreSQL simultaneous

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,49 @@
+import { PrismaClient } from "@prisma/client";
 import { envs } from "./config/plugins/envs.plugin";
+import { MongoDataBase } from "./data/mongoDB";
 import { Server } from "./presentation/server";
 
 ( async () => {
     main();
 })();
 
-function main() {
-    Server.start();
+async function main() {
+    await MongoDataBase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    });
+    //!MONGO
+    // await newLog.save();
+    // const logs = await LogModel.find();
+    // console.log(logs);
+
+    //!PRISMA
+    // const prisma = new PrismaClient();
+
+    // const newLog = await prisma.logModel.create({
+    //     data: {
+    //         level: 'HIGH',
+    //         message: 'Text message',
+    //         origin: 'App.ts'
+    //     }
+    // });
+
+    // const logs = await prisma.logModel.findMany({
+    //     where: {
+    //         level: 'MEDIUM'
+    //     }
+    // });
+
+    // const newLog = await LogModel.create({
+    //     message: 'Test Message 232',
+    //     origin: 'App.ts',
+    //     level: 'medium'
+    // });
+
+    
+
     // console.log(envs);
+
+
+    Server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,21 +1,34 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDataSource } from "../infrastructure/datasources/postgre-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(
+const fsLogRepository = new LogRepositoryImpl(
     new FileSystemDatasource(),
-    // new postgresSQLLogDataSource(),
-    // new mongoLogDTS(),
+    // new MongoLogDatasource(),
+    // new PostgresLogDataSource(),
+);
+const mongoLogRepository = new LogRepositoryImpl(
+    // new FileSystemDatasource(),
+    new MongoLogDatasource(),
+    // new PostgresLogDataSource(),
+);
+const postgresLogRepository = new LogRepositoryImpl(
+    // new FileSystemDatasource(),
+    // new MongoLogDatasource(),
+    new PostgresLogDataSource(),
 );
 
 export class Server {
     public static start() {
-        console.log('Server started...');
+        console.log('Server started..asda....');
 
         //Email
-        const emailService = new EmailService();
+        // const emailService = new EmailService();
         // emailService.sendEmail({
         //     to: 'urissent@gmail.com',
         //     subject: 'TESTING',
@@ -26,18 +39,17 @@ export class Server {
         //     `
         // });
 
-        emailService.sendEmailWithFileSystemLogs(['urissent@gmail.com', 'alexdetailsv@gmail.com']);
+        // emailService.sendEmailWithFileSystemLogs(['urissent@gmail.com', 'alexdetailsv@gmail.com']);
 
         // CronService.createdJob(
         //     '*/5 * * * * *',
         //     () => {
-        //         const url = 'http://google.com';
-        //         new CheckService(
-        //             fileSystemLogRepository,
+        //         const url = 'http://googaasle.com';
+        //         new CheckServiceMultiple(
+        //             [fsLogRepository, mongoLogRepository, postgresLogRepository],
         //             () => console.log(`${url} is ok` ),
         //             (error) => console.log(error),
         //         ).execute(url);
-        //         // new CheckService().execute('http://localhost:3000');
         //     }
         // );
 
